1adb337ed211e57a9555eaa15d593ea94570ae89
Het Patel <hetpatel3245@gmail.com>
2023-09-10 11:17:54 +0530

    NewsApp using class-based-components
    temporary stage news app has been completed. Frontend improvement and adding some features remains to be done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
+/**** App is created using class based components *****/
+
 import './App.css';
 
 import React, { Component } from 'react'
 import News from './Components/News';
 import Navbar from './Components/Navbar';
+import LoadingBar from 'react-top-loading-bar'
+
 
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link,
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
 } from "react-router-dom";
 
 export default class App extends Component {
+   apikey = process.env.REACT_APP_NEWS_API
+   state = {
+      progress: 0
+   }
+
+   setProgress = (progress)=>{
+      this.setState({progress : progress});
+   }
+
   render() {
     return (
       <div>
+        
         <BrowserRouter>
-          <Navbar />
+        <Navbar />
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress= {this.state.progress}
+          />
           {/* For fourcefully mount the news components here key props is used */}
           <Routes>
-            <Route  path="/" element={<News key="general" pageSize={10} country={'in'} category="general" />} />
-            <Route  path="/business" element={<News key="business" pageSize={10} country={'in'} category="business" />} />
-            <Route  path="/entertainment" element={<News key="entertainment" pageSize={10} country={'in'} category="entertainment" />} />
-            <Route  path="/health" element={<News key="health"  pageSize={10} country={'in'} category="health" />} />
-            <Route  path="/science" element={<News key="science" pageSize={10} country={'in'} category="science" />} />
+            <Route path="/" element={<News apikey={this.apikey} setProgress={this.setProgress} setkey="general" pageSize={10} country={'in'} category="general" />} />
+            <Route path="/business" element={<News apikey={this.apikey} setProgress={this.setProgress} key="business" pageSize={10} country={'in'} category="business" />} />
+            <Route path="/entertainment" element={<News apikey={this.apikey} setProgress={this.setProgress} key="entertainment" pageSize={10} country={'in'} category="entertainment" />} />
+            <Route path="/health" element={<News apikey={this.apikey} setProgress={this.setProgress} key="health" pageSize={10} country={'in'} category="health" />} />
+            <Route path="/science" element={<News apikey={this.apikey} setProgress={this.setProgress} key="science" pageSize={10} country={'in'} category="science" />} />
           </Routes>
-          
+
         </BrowserRouter>
       </div>
     )
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,33 +9,34 @@ import {
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <Link className="navbar-brand" to="#">Navbar</Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item active">
-                                <Link exact className="nav-link" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link exact className="nav-link" to="/business">Business</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link exact className="nav-link" to="/entertainment">Entertainment</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link exact className="nav-link" to="/health">Health</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link exact className="nav-link" to="/science">Science</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3 py-2">
+                 <div class="container-fluid">
+                <Link className="navbar-brand" to="/">NewsApp</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/business">Business</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/health">Health</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/science">Science</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
+            </nav>
+
         )
     }
 }
